d44effb20f22f33dcccb693629c6f3a2566ed50f
jamorr <Morris.Jack@outlook.com>
2023-12-07 10:30:43 -0500

    model weights

---
+++ src/sim_clr_test.py
@@ -0,0 +1,89 @@
+
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+import torchvision
+from PIL import Image
+from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import normalize
+
+from lightly.data import LightlyDataset
+from lightly.transforms import SimCLRTransform, utils
+
+from lightly.loss import NTXentLoss
+from lightly.models.modules.heads import SimCLRProjectionHead
+
+from sim_clr import SimCLRModel, generate_embeddings
+
+
+def get_image_as_np_array(filename: str):
+    """Returns an image as an numpy array"""
+    img = Image.open(filename)
+    return np.asarray(img)
+
+
+def plot_knn_examples(embeddings, filenames, n_neighbors=3, num_examples=6):
+    """Plots multiple rows of random images with their nearest neighbors"""
+    # lets look at the nearest neighbors for some samples
+    # we use the sklearn library
+    nbrs = NearestNeighbors(n_neighbors=n_neighbors).fit(embeddings)
+    distances, indices = nbrs.kneighbors(embeddings)
+
+    # get 5 random samples
+    samples_idx = np.random.choice(len(indices), size=num_examples, replace=False)
+
+    # loop through our randomly picked samples
+    for idx in samples_idx:
+        fig = plt.figure()
+        # loop through their nearest neighbors
+        for plot_x_offset, neighbor_idx in enumerate(indices[idx]):
+            # add the subplot
+            ax = fig.add_subplot(1, len(indices[idx]), plot_x_offset + 1)
+            # get the correponding filename for the current index
+            fname = os.path.join(path_to_data, filenames[neighbor_idx])
+            # plot the image
+            plt.imshow(get_image_as_np_array(fname))
+            # set the title to the distance of the neighbor
+            ax.set_title(f"d={distances[idx][plot_x_offset]:.3f}")
+            # let's disable the axis
+            plt.axis("off")
+        plt.savefig(f'../results/knn_{idx}.png')
+
+path_to_weights = '../models/sim_clr_100.pth'
+path_to_data = "../lemon-dataset/images"
+num_workers = 4
+batch_size = 256
+seed = 1
+max_epochs = 100
+input_size = 128
+num_ftrs = 32
+# We create a torchvision transformation for embedding the dataset after
+# training
+test_transform = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.Resize((input_size, input_size)),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(
+            mean=utils.IMAGENET_NORMALIZE["mean"],
+            std=utils.IMAGENET_NORMALIZE["std"],
+        ),
+    ]
+)
+dataset_test = LightlyDataset(input_dir=path_to_data, transform=test_transform)
+dataloader_test = torch.utils.data.DataLoader(
+    dataset_test,
+    batch_size=batch_size,
+    shuffle=False,
+    drop_last=False,
+    num_workers=num_workers,
+)
+model = SimCLRModel()
+model.load_state_dict(torch.load(path_to_weights))
+model.eval()
+embeddings, filenames = generate_embeddings(model, dataloader_test)
+
+plot_knn_examples(embeddings, filenames)
